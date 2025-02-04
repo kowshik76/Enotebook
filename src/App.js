96cd727c064f5594ac9dd3c09b423f3b90f7457e
kowshik76 <kowshiksangada@gmailcom>
2025-02-04 14:34:13 +0530

    Added Components and bootsrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Home from './components/Home';
 function App() {
   return (
-    <div className="App">
-      <h1>This is enoteBook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+
+
+        <Routes>
+          <Route
+            exact
+            path="/home"
+            element={<Home />}
+          />
+
+
+          <Route
+            exact
+            path="/about"
+            element={<About />}
+          />
+
+        </Routes>
+      </Router>
+    </>
   );
 }
 
