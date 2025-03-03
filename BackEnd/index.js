049190aed400ebceaef4603a0d9a82e2b644f0bc
kowshik76 <kowshiksangada@gmail.com>
2025-03-03 22:11:17 +0530

    Bugs fixed

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -34,7 +34,7 @@ app.use('/api/notes', require('./routes/notes'));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../build/index.html'));
+        res.sendFile(path.join(__dirname, '../build/index.html')); // Fixed: Removed extra parenthesis
     });
 }
 
